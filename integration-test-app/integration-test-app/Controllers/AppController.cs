adf07402097133526095a23588edf32c0d58e07c
Bryan Aguilar <bryaag@amazon.com>
2022-06-06 09:15:53 -0700

    Add mock metrics

--- integration-test-app/integration-test-app/Controllers/AppController.cs
+++ integration-test-app/integration-test-app/Controllers/AppController.cs
@@ -1,7 +1,9 @@
-﻿using Amazon.S3;
+﻿using System;
+using Amazon.S3;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using System.Net.Http;
+using Microsoft.AspNetCore.Http.Extensions;
 
 namespace integration_test_app.Controllers
 {
@@ -11,13 +13,23 @@ namespace integration_test_app.Controllers
     {
         private readonly AmazonS3Client s3Client = new AmazonS3Client();
         private readonly HttpClient httpClient = new HttpClient();
+        private readonly MetricEmitter _metricEmitter = new MetricEmitter();
+        private static Random rand = new Random(DateTime.Now.Millisecond);
 
         [HttpGet]
         [Route("/outgoing-http-call")]
         public string OutgoingHttp()
         {
-            _ = httpClient.GetAsync("https://aws.amazon.com").Result;
-
+            var res = httpClient.GetAsync("https://aws.amazon.com").Result;
+            string statusCode = res.StatusCode.ToString();
+            // mimic latency for now
+            _metricEmitter.emitReturnTimeMetric(MimicLatency(),Request.GetDisplayUrl(),statusCode);
+            
+            // emit load size
+            int loadSize = MimicPayLoadSize();
+            _metricEmitter.emitBytesSentMetric(loadSize,Request.GetDisplayUrl(),statusCode);
+            _metricEmitter.updateTotalBytesSentMetric(loadSize, Request.GetDisplayUrl(),statusCode);
+            
             return GetTraceId();
         }
 
@@ -44,6 +56,16 @@ namespace integration_test_app.Controllers
             var epoch = traceId.Substring(0, 8);
             var random = traceId.Substring(8);
             return "{" + "\"traceId\"" + ": " + "\"" + version + "-" + epoch + "-" + random + "\"" + "}";
+        }
+
+        private static int MimicPayLoadSize()
+        {
+            return rand.Next(101);
+        }
+
+        private static int MimicLatency()
+        {
+            return rand.Next(100,5000);
         }
     }
 
